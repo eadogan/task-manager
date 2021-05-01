cf7f1a67731fed06b39e9478b57eb5ebdcf5ed25
eadogan <e.ahmetdogan@gmail.com>
2021-05-02 00:55:03 +0100

    Refactor User Router

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 require('./db/mongoose')
-const User = require('./models/User')
 const Task = require('./models/Task')
 const userRouter = require('./routers/user')
 
@@ -10,82 +9,7 @@ const port = process.env.PORT || 3000
 app.use(express.json())
 app.use(userRouter)
 
-// Users
 
-app.post('/users', async (req, res) => {
-    const user = new User(req.body)
-
-    try {
-        await user.save()
-        res.status(201).send(user)
-    } catch (error) {
-        res.status(400).send(error)
-    }
-})
-
-app.get('/users/:id', async (req, res) => {
-    const _id = req.params.id
-
-    try {
-        const user = await User.findById(_id)
-        if(!user) {
-            return res.status(404).send()
-        }
-        res.status(200).send(user)
-    } catch (error) {
-        res.status(400).send(error)
-    }
-})
-
-app.get('/users', async (req, res) => {
-
-    try {
-        const user = await User.find({})
-        if(!user) {
-            return res.status(404).send()
-        }
-        res.status(200).send(user)
-    } catch (error) {
-        res.status(400).send(error)
-    }
-
-})
-
-app.patch('/users/:id', async (req, res) => {
-
-    const updates = Object.keys(req.body)
-    const allowedUpdates = ['name', 'email', 'password', 'age']
-    const isValidOperation = updates.every((update) => allowedUpdates.includes(update))
-
-    if(!isValidOperation){
-        return res.status(400).send({error: 'Invalid User field!'})
-    }
-
-    try {
-        const user = await User.findByIdAndUpdate(req.params.id, req.body, {new: true, runValidators: true})
-
-        if(!user) {
-            return res.status(404).send()
-        }
-        res.status(200).send(user)
-    } catch (error) {
-        res.status(400).send(error)
-    }
-})
-
-app.delete('/users/:id', async (req, res) => { 
-
-    try {
-        const user = await User.findByIdAndDelete(req.params.id)
-
-        if(!user) {
-            return res.status(404).send()
-        }
-        res.status(200).send(user)
-    } catch (error) {
-        res.status(400).send(error)
-    }
-})
 
 // Tasks
 
