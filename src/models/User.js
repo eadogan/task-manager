eb02ec03c21c731351889e5d7f0508904f4b7835
eadogan <e.ahmetdogan@gmail.com>
2021-05-02 12:28:47 +0100

    Hash password via bcryptjs

--- src/models/User.js
+++ src/models/User.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcryptjs')
 const validator = require('validator')
 
-const User = mongoose.model('User', {
+const userSchema = new mongoose.Schema({
     name: {
         type: String,
         required: true,
@@ -35,5 +36,19 @@ const User = mongoose.model('User', {
         }
     }
 })
+
+
+// Middleware - Hash Password
+userSchema.pre('save', async function (next) {
+    const user = this
+
+    if(user.isModified('password')) {
+        user.password = await bcrypt.hash(user.password, 8)
+    }
+
+    next()
+})
+
+const User = mongoose.model('User', userSchema)
 
 module.exports = User
