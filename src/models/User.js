20947ea79aba7bb6d08fa70dc33741952c39151d
eadogan <e.ahmetdogan@gmail.com>
2021-05-03 10:11:31 +0100

    Create generateAuthToken for Authorization

--- src/models/User.js
+++ src/models/User.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose')
 const bcrypt = require('bcryptjs')
 const validator = require('validator')
+const jwt = require('jsonwebtoken')
 
 const userSchema = new mongoose.Schema({
     name: {
@@ -9,6 +10,7 @@ const userSchema = new mongoose.Schema({
         trim: true
     }, email: {
         type: String,
+        unique: true,
         required: true,
         trim: true,
         validate(value){
@@ -34,8 +36,37 @@ const userSchema = new mongoose.Schema({
                 throw new Error('Age must be a positive number')
             }
         }
-    }
+    }, tokens: [{
+        token: {
+            type: String,
+            required: true
+        }
+    }]
 })
+
+userSchema.methods.generateAuthToken = async function () {
+    const user = this
+
+    // payload and secret key
+    const token = jwt.sign({ _id: user._id.toString() }, 'taskmanagerapi')
+    user.tokens = user.tokens.concat({ token })
+    await user.save()
+    return token
+}
+
+userSchema.statics.findByCredentials = async (email, password) => {
+    const user  = await User.findOne({email})
+    if(!user) {
+        throw new Error('Unable to login!')
+    }
+    const isMatch = await bcrypt.compare(password, user.password)
+
+    if(!isMatch) {
+        throw new Error('Unable to login!')
+    }
+
+    return user
+}
 
 
 // Middleware - Hash Password
