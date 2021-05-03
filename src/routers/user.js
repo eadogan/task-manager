1b34d9e3694d620e17eef3da9d79dc95ed89a819
eadogan <e.ahmetdogan@gmail.com>
2021-05-03 10:10:02 +0100

    Refactor existing router via auth

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,14 +1,26 @@
 const express = require('express')
 const router = new express.Router()
 const User = require('../models/User')
+const auth = require('../middleware/auth')
 
 // Users
+router.post('/users/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        const token = await user.generateAuthToken()
+        res.send({ user, token })
+    } catch (error) {
+        res.status(400).send(error)
+    }
+})
+
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
 
     try {
         await user.save()
-        res.status(201).send(user)
+        const token = await user.generateAuthToken()
+        res.status(201).send({ user, token })
     } catch (error) {
         res.status(400).send(error)
     }
@@ -28,18 +40,9 @@ router.get('/users/:id', async (req, res) => {
     }
 })
 
-router.get('/users', async (req, res) => {
+router.get('/users', auth, async (req, res) => {
 
-    try {
-        const user = await User.find({})
-        if(!user) {
-            return res.status(404).send()
-        }
-        res.status(200).send(user)
-    } catch (error) {
-        res.status(400).send(error)
-    }
-
+    res.status(201).send(req.user)
 })
 
 router.patch('/users/:id', async (req, res) => {
