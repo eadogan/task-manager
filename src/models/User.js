b28c94611758f036793d1e8b2326de2e9af3fedb
eadogan <e.ahmetdogan@gmail.com>
2021-05-05 02:15:57 +0100

    Add user avatar

--- src/models/User.js
+++ src/models/User.js
@@ -42,7 +42,9 @@ const userSchema = new mongoose.Schema({
             type: String,
             required: true
         }
-    }]
+    }], avatar: {
+        type: Buffer
+    }
 }, {
     timestamps: true
 })
