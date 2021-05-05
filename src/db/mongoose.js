57857983136b44f7a139fb5fc849bee441509686
eadogan <e.ahmetdogan@gmail.com>
2021-05-05 12:42:34 +0100

    Change config file

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,9 +1,8 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb://localhost:27017/task-manager', {
+mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
     useFindAndModify: false
 })
-
--- src/index.js
+++ src/index.js
@@ -5,11 +5,11 @@ const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT 
 
 app.use(express.json())
 app.use(userRouter, taskRouter)
 
 app.listen(port, () => {
-    console.log('Server is on port '+port)
+    console.log(`Server is on port ${port}`)
 })
